5f8cca47b63dab7a91c61c3c4ba05950ef976fe6
Nicolas Garcia Sastre <n.garcia.sastre@gmail.com>
2024-12-07 16:32:59 +0100

    fixed translations

--- src/i18n.js
+++ src/i18n.js
@@ -4,6 +4,8 @@ import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';  
 import Backend from 'i18next-http-backend';  // Cargar las traducciones desde archivos
 
+const baseUrl = process.env.PUBLIC_URL || '';
+
 i18n
   .use(Backend) // Usar Backend para cargar archivos
   .use(LanguageDetector) 
@@ -12,7 +14,7 @@ i18n
     fallbackLng: 'en',
     debug: true,
     backend: {
-      loadPath: '/locales/{{lng}}/translation.json', // Ruta para cargar los archivos JSON
+      loadPath: `${baseUrl}/locales/{{lng}}/translation.json`, // Ruta para cargar los archivos JSON
     },
     interpolation: {
       escapeValue: false
